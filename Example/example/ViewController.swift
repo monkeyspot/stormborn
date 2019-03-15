4df37b997471c4764ce78a57bbb9985ee4bde639
Oliver Letterer <oliver.letterer@gmail.com>
2019-03-15 09:37:46 +0100

    Adding runtime support for multiple swizzled members.

--- Example/example/ViewController.swift
+++ Example/example/ViewController.swift
@@ -9,12 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("original")
     }
-
-
 }
-
--- stormborn/runtime.m
+++ stormborn/runtime.m
@@ -9,6 +9,7 @@
 #import "runtime.h"
 #import "SWZInvocation.h"
 #import <objc/message.h>
+#import <pthread.h>
 
 _Nullable Method swizzle_lookupMember(Class klass, NSString *member, NSInteger arguments, BOOL returns)
 {
@@ -36,10 +37,26 @@ _Nullable Method swizzle_lookupMember(Class klass, NSString *member, NSInteger a
 
 void swizzle_hookMember(Class klass, NSString *member, NSInteger arguments, BOOL returns, void(^block)(id object, SWZInvocation *invocation))
 {
+    static pthread_mutex_t mutex;
+    
+    static NSMutableDictionary<NSString *, NSMutableDictionary<NSString *, NSMutableArray<id> *> *> *blockLookup = nil;
+    static NSMutableDictionary<NSString *, NSMutableDictionary<NSString *, NSMutableArray<NSString *> *> *> *implementationLookup = nil;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        pthread_mutex_init(&mutex, NULL);
+        
+        blockLookup = [NSMutableDictionary dictionary];
+        implementationLookup = [NSMutableDictionary dictionary];
+    });
+    
+    pthread_mutex_lock(&mutex);
+    
     Method method = swizzle_lookupMember(klass, member, arguments, returns);
     assert(method != NULL);
     
-    SEL selector = NSSelectorFromString([NSString stringWithFormat:@"_swizzle_%@_%@", NSUUID.UUID.UUIDString, NSStringFromSelector(method_getName(method))]);
+    NSString *uuidPrefix = NSUUID.UUID.UUIDString;
+    SEL selector = NSSelectorFromString([NSString stringWithFormat:@"_swizzle_%@_%@", uuidPrefix, NSStringFromSelector(method_getName(method))]);
     class_addMethod(klass, selector, method_getImplementation(method), method_getTypeEncoding(method));
     
     BOOL hasDirectImplementation = method == class_getInstanceMethod(klass, method_getName(method));
@@ -65,25 +82,39 @@ void swizzle_hookMember(Class klass, NSString *member, NSInteger arguments, BOOL
         class_addMethod(klass, @selector(methodSignatureForSelector:), imp_implementationWithBlock(methodSignatureForSelector), method_getTypeEncoding(class_getInstanceMethod(NSObject.class, @selector(methodSignatureForSelector:))));
     }
     
+    NSString *forwardInvocationSelector = [NSString stringWithFormat:@"_swizzle_%@_%@", uuidPrefix, NSStringFromSelector(@selector(forwardInvocation:))];
     IMP originalforwardInvocation = [klass instanceMethodForSelector:@selector(forwardInvocation:)];
+    
+    __block NSArray<NSString *> *theseImplementations = nil;
     void(^forwardInvocation)(__unsafe_unretained id object, NSInvocation *invocation) = ^(__unsafe_unretained id object, NSInvocation *invocation) { // dealloc support
-        if (invocation.selector == method_getName(method)) {
-            NSInvocation *newInvocation = [NSInvocation invocationWithMethodSignature:invocation.methodSignature];
+        if (invocation.selector == method_getName(method) || [theseImplementations containsObject:NSStringFromSelector(invocation.selector)]) {
+            NSInvocation *sup = [NSInvocation invocationWithMethodSignature:invocation.methodSignature];
+            sup.target = invocation.target;
             
-            newInvocation.target = invocation.target;
-            newInvocation.selector = selector;
+            void(^thisBlock)(id object, SWZInvocation *invocation) = nil;
             
-            for (NSInteger i = 2; i < newInvocation.methodSignature.numberOfArguments; i++) {
-                void *argument = NULL;
-                [invocation getArgument:&argument atIndex:i];
-                [newInvocation setArgument:&argument atIndex:i];
+            if (theseImplementations.count == 0 || invocation.selector == method_getName(method)) {
+                sup.selector = selector;
+                thisBlock = blockLookup[NSStringFromClass(klass)][member].lastObject;
+            } else {
+                NSInteger index = [theseImplementations indexOfObject:NSStringFromSelector(invocation.selector)];
+                assert(index != NSNotFound);
+                
+                sup.selector = NSSelectorFromString(theseImplementations[index - 1]);
+                thisBlock = [blockLookup[NSStringFromClass(klass)][member] objectAtIndex:index - 1];
             }
             
-            block(object, [[SWZInvocation alloc] initWithInvocation:newInvocation]);
+            for (NSInteger i = 2; i < sup.methodSignature.numberOfArguments; i++) {
+                void *argument = NULL;
+                [invocation getArgument:&argument atIndex:i];
+                [sup setArgument:&argument atIndex:i];
+            }
+            
+            thisBlock(object, [[SWZInvocation alloc] initWithInvocation:sup]);
             
             if (![@(invocation.methodSignature.methodReturnType) isEqualToString:@"v"]) {
                 void *returnValue = NULL;
-                [newInvocation getReturnValue:&returnValue];
+                [sup getReturnValue:&returnValue];
                 [invocation setReturnValue:&returnValue];
                 
                 [invocation retainArguments];
@@ -101,4 +132,29 @@ void swizzle_hookMember(Class klass, NSString *member, NSInteger arguments, BOOL
     } else {
         class_addMethod(klass, @selector(forwardInvocation:), imp_implementationWithBlock(forwardInvocation), method_getTypeEncoding(class_getInstanceMethod(NSObject.class, @selector(forwardInvocation:))));
     }
+    
+    class_addMethod(klass, NSSelectorFromString(forwardInvocationSelector), imp_implementationWithBlock(forwardInvocation), method_getTypeEncoding(class_getInstanceMethod(NSObject.class, @selector(forwardInvocation:))));
+    
+    {
+        NSMutableDictionary<NSString *, NSMutableArray<NSString *> *> *memberLookup = implementationLookup[NSStringFromClass(klass)] ?: [NSMutableDictionary dictionary];
+        implementationLookup[NSStringFromClass(klass)] = memberLookup;
+        
+        NSMutableArray<NSString *> *lookup = memberLookup[member] ?: [NSMutableArray array];
+        memberLookup[member] = lookup;
+        
+        [lookup addObject:NSStringFromSelector(selector)];
+        theseImplementations = lookup.copy;
+    }
+    
+    {
+        NSMutableDictionary<NSString *, NSMutableArray<id> *> *memberLookup = blockLookup[NSStringFromClass(klass)] ?: [NSMutableDictionary dictionary];
+        blockLookup[NSStringFromClass(klass)] = memberLookup;
+        
+        NSMutableArray<id> *lookup = memberLookup[member] ?: [NSMutableArray array];
+        memberLookup[member] = lookup;
+        
+        [lookup addObject:[block copy]];
+    }
+    
+    pthread_mutex_unlock(&mutex);
 }
